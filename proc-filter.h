bb85f7064c39d0e3e88d205650c93aca4e5c53f2
Tert-butyllithium <lihaonan1105@gmail.com>
2021-04-16 07:35:42 +0000

    update: add filter condition: ppid and parent_proc_name

--- proc-filter.h
+++ proc-filter.h
@@ -8,6 +8,13 @@ unsigned check_proc_exclusive(char*);
 
 u8 check_proc_pid(int pid) { return pid == current->pid; }
 
+u8 check_proc_ppid(int ppid) {
+  if (unlikely(!current->parent)) {
+    return 0;
+  }
+  return pid == current->parent->pid;
+}
+
 // only get first 15 characters
 char* get_process_name() { return current->comm; }
 
@@ -20,12 +27,30 @@ u8 check_proc_name(char* proc_name) {
   return strncmp(cur_proc_name, proc_name, cmp_max_len) == 0;
 }
 
-u8 check_proc(int pid, char* proc_name) {
+u8 check_parent_proc_name(char* proc_name) {
+  char* cur_proc_name;
+  int cmp_max_len;
+  if (unlikely(!current->pid)) {
+    return 0;
+  }
+  cur_proc_name = current->parent->comm;
+  cmp_max_len = min(strlen(proc_name), MAX_STR_LENGTH);
+  return strncmp(cur_proc_name, proc_name, cmp_max_len) == 0;
+}
+
+u8 check_proc(int pid, char* proc_name, int ppid, char* parent_proc_name) {
   if (pid) {
     return check_proc_pid(pid);
   }
   if (strcmp(proc_name, "")) {
     return check_proc_name(proc_name);
+  }
+
+  if (ppid) {
+    return check_proc_ppid(ppid);
+  }
+  if (strcmp(parent_proc_name, "")) {
+    return check_parent_proc_name(parent_proc_name);
   }
   return 1;
 }
