4915a93ff34c27200f6a435311bbda2ba9400531
Tert-butyllithium <lihaonan1105@gmail.com>
2021-05-07 02:39:01 +0000

    prevent write to file during sendfile

--- syscalls/handlers-table.h
+++ syscalls/handlers-table.h
@@ -11,7 +11,7 @@ handler_callback functions[] = {
     &tgkill_handle, &munmap_handle,   &nanosleep_handle, &ppoll_handle,
     &dup_handle,    &ioctl_handle,    &open_handle,      &creat_handle,
     &openat_handle, &close_handle,    &write_handle,     &write_handle,
-    &write_handle, &write_handle};
+    &write_handle, &write_handle, &write_handle};
 
 char handler_string[][32] = {
     "getuid", "recvfrom",   "socket", "fstat",  "getcwd",
